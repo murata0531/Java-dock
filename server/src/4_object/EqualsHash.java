07928780e846990f5ea6e6dc93ce5fe5680a1c27
murata0531 <coless115@gmail.com>
2021-03-03 01:46:37 +0900

    equals hashcode override

--- server/src/4_object/EqualsHash.java
+++ server/src/4_object/EqualsHash.java
@@ -14,5 +14,13 @@ class Foo{
     public int hashCode(){
         return num * 5;
     }
+}
 
+public class EqualsHash {
+    public static void main(String[] args){
+        Foo f1 = new Foo();
+        Foo f2 = new Foo();
+
+        System.out.println("f1.equals(f2) : " + (f1.equals(f2)));
+    }
 }
