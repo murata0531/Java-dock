f90b17f874d57543ec11df1a0b897feb9a93e325
murata0531 <coless115@gmail.com>
2021-03-04 01:18:13 +0900

    private method in the interface

---
+++ server/src/5_interface/PrivateInterface.java
@@ -0,0 +1,9 @@
+interface Foo {
+    private static void p1(){
+        System.out.println("Foo : p1()");
+    }
+
+    private static void p2(){
+        System.out.println("Foo : p2()");
+    }
+}
