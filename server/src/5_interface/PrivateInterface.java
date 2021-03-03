c2391a8c0a0436693020785b40cd4a024a5e5f91
murata0531 <coless115@gmail.com>
2021-03-04 01:23:22 +0900

    private access in the interface

--- server/src/5_interface/PrivateInterface.java
+++ server/src/5_interface/PrivateInterface.java
@@ -6,4 +6,16 @@ interface Foo {
     private static void p2(){
         System.out.println("Foo : p2()");
     }
+
+    static void methodA(){
+        System.out.println("Foo : methodA()");
+        p1();
+        //p2(); コンパイルエラー
+    }
+
+    default void methodB(){
+        System.out.println("Foo : methodB()");
+        p1();
+        p2();
+    }
 }
