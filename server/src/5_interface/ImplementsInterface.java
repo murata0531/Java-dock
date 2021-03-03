fa4d0034265e599ac8209d0c6b7169ce3cec1eda
murata0531 <coless115@gmail.com>
2021-03-04 01:42:41 +0900

    implements interface class

--- server/src/5_interface/ImplementsInterface.java
+++ server/src/5_interface/ImplementsInterface.java
@@ -11,3 +11,16 @@ interface MyInter2 {
         System.out.println("methodD");
     }
 }
+
+class MyClass implements MyInter1, MyInter2 {
+
+    @override
+    public double methodA(int num){
+        return num * 0.3;
+    }
+
+    @override
+    public int methodC(int val1, int val2){
+        return val1 + val2;
+    }
+}
