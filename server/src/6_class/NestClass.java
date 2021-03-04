e94d798a9310c07fd0c38a9cd117682ff53b3259
murata0531 <coless115@gmail.com>
2021-03-05 01:47:06 +0900

    static classes cannot access outer instance variables

--- server/src/6_class/NestClass.java
+++ server/src/6_class/NestClass.java
@@ -9,13 +9,35 @@ class Outer {
         //non-static method
         void method1(){
             System.out.println("A#method1()#instance val1 : " + val1);
-            System.out.println("A#method1()#static val2 : " + val1);
+            System.out.println("A#method1()#static val2 : " + val2);
 
             // this is Compile error because non-static classes cannot have static members
             // static void method2(){
             //     System.out.println("A#method2()#instance val1 : " + val1);
             //     System.out.println("A#method2()#static val2 : " + val1);
             // }
+        }
+    }
+
+    //static inner class
+    static class B{
+
+        //non-static method
+        void method1(){
+
+            //this is Compile error because static classes cannot access outer instance variables
+            // System.out.println("B#method1()#instance val1 : " + val1);
+
+            System.out.println("B#method1()#static val2 : " + val2);
+
+            //static method
+            static void method2(){
+
+                //this is Compile error because static classes cannot access outer instance variables
+                // System.out.println("B#method2()#instance val1 : " + val1);
+                System.out.println("B#method2()#static val2 : " + val2);
+
+            }
         }
     }
 }
