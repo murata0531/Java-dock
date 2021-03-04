423afa7abfc3344682d52e227d845a8c1ae34ad8
murata0531 <coless115@gmail.com>
2021-03-05 01:35:13 +0900

    non-static classes cannot have static members

--- server/src/6_class/NestClass.java
+++ server/src/6_class/NestClass.java
@@ -3,14 +3,19 @@ class Outer {
     private int val1 = 100;
     private static int val2 = 200;
 
-    //no static inner class
+    //non-static inner class
     class A {
 
-        //no static method
+        //non-static method
         void method1(){
             System.out.println("A#method1()#instance val1 : " + val1);
             System.out.println("A#method1()#static val2 : " + val1);
 
+            // this is Compile error because non-static classes cannot have static members
+            // static void method2(){
+            //     System.out.println("A#method2()#instance val1 : " + val1);
+            //     System.out.println("A#method2()#static val2 : " + val1);
+            // }
         }
     }
 }
