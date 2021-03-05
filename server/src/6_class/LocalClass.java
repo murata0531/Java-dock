38bb09688f46c6e6fae7aca4582843e9778e521e
murata0531 <coless115@gmail.com>
2021-03-06 02:45:08 +0900

    local class

---
+++ server/src/6_class/LocalClass.java
@@ -0,0 +1,28 @@
+class Outer {
+
+    private static int a = 1;
+    private int b = 2;
+
+    void methodOuter(final int c, int d){
+
+        final int e = 5;
+        int f = 6;
+
+        class A {
+            void method(){
+                System.out.println(a + "");
+                System.out.println(b + "");
+                System.out.println(c + "");
+                System.out.println(d + "");
+                System.out.println(e + "");
+                System.out.println(f + "");
+
+                //this is compile error because  these variables are effectively final
+                // d = 100;
+                // f = 100;
+            }
+        }
+
+        new A().method();
+    }
+}
