99aa0b54761bdc226dd9c07f0ef43fa5e623c64e
murata0531 <coless115@gmail.com>
2021-03-07 02:53:25 +0900

    suppress worning

--- server/src/7_Annotation/SuppressWarnings.java
+++ server/src/7_Annotation/SuppressWarnings.java
@@ -1,8 +1,8 @@
 import java.util.*;
 
 public class SuppressWarnings {
-    @SuppressWarnings(value = {"unchecked","deprecation"})
-    // @SuppressWarnings({"unchecked","deprecation"})
+    @java.lang.SuppressWarnings(value = {"unchecked","deprecation"})
+    // @java.lang.SuppressWarnings({"unchecked","deprecation"})
     public static void main(String[] args) {
 
         Date date = new Date("2021/03/15");
