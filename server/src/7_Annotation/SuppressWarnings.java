05bb291c36f3068a3809321823b1bec302dcc7dc
murata0531 <coless115@gmail.com>
2021-03-07 02:14:04 +0900

    suppres warnings

---
+++ server/src/7_Annotation/SuppressWarnings.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class SuppressWarnings {
+    @SuppressWarnings(value = {"unchecked","deprecation"})
+    @SuppressWarnings({"unchecked","deprecation"})
+    public static void main(String[] args) {
+
+        Date date = new Date("2021/03/15");
+        List list = new ArrayList();
+    }
+}
