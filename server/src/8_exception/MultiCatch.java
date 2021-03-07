631ba36c01352de6841a6db0e76af8753d499b67
murata0531 <coless115@gmail.com>
2021-03-08 01:55:26 +0900

    multi-catch

--- server/src/8_exception/MultiCatch.java
+++ server/src/8_exception/MultiCatch.java
@@ -8,7 +8,9 @@ public class MultiCatch {
             System.out.println(Integer.parseInt(s));
             System.out.println(num[0] / num[1]);
         }catch(NumberFormatException | ArithmeticException e){
-            e.printStacTrace();
+            e.printStackTrace();
+            //this is compile error because 'e' is Implicit final
+            // e = null;
         }
     }
 }
