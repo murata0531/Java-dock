65da6e67fd0d444af5175e068622e0452d721eb0
murata0531 <coless115@gmail.com>
2021-03-09 01:56:57 +0900

    rethrow

--- server/src/8_exception/ReThrow.java
+++ server/src/8_exception/ReThrow.java
@@ -11,6 +11,25 @@ class MyExceptionB extends Exception {
 }
 public class ReThrow {
     public static void main(String[] args){
+        try {
+            method();
+        }catch(MyExceptionA | MyExceptionB e){
+            System.out.println(e.getMessage());
+        }
+    }
 
+    public static void method() throws MyExceptionA,MyExceptionB {
+
+        int num = (int)(Math.random() * 10);
+
+        try {
+            if(num < 4){
+                throw new MyExceptionA();
+            }else {
+                throw new MyExceptionB();
+            }
+        }catch(Exception e){
+            throw e;
+        }
     }
 }
