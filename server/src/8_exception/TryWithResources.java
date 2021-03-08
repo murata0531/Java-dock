fcc5272918b9d9f001ab586584fdcafb6cf608a9
murata0531 <coless115@gmail.com>
2021-03-09 02:13:06 +0900

    try with resources

--- server/src/8_exception/TryWithResources.java
+++ server/src/8_exception/TryWithResources.java
@@ -16,5 +16,16 @@ class MyResource implements AutoCloseable {
 public class TryWithResources {
     public static void main(String[] args){
 
+        try(MyResource obj1 = new MyResource("obj1");
+            MyResource obj2 = new MyResource("obj2")){
+            System.out.println("try block");
+            throw new SQLException();
+        }catch(SQLException e){
+            System.out.println("catch block : SQLException");
+        }catch(Exception e){
+            System.out.println("catch block : Exception");
+        }finally {
+            System.out.println("finally block");
+        }
     }
 }
