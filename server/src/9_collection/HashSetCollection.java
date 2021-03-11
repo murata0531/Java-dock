d643696b82a9ae4b7a5dcce4cb2cdb9a1cec0a03
murata0531 <coless115@gmail.com>
2021-03-12 01:48:53 +0900

    HashSet

---
+++ server/src/9_collection/HashSetCollection.java
@@ -0,0 +1,23 @@
+import java.util.HshSet;
+
+public class HashSetCollection {
+    public static void main(String[] args){
+
+        String[] ary = {"CCCC","AAA","BBB"};
+        HashSet<String> hashSet = new HashSet<String>();
+        hashSet.add(ary[0]);
+        hashSet.add(ary[1]);
+        hashSet.add(ary[2]);
+        hashSet.add(ary[0]);
+
+        System.out.println("HashSet size : " + hashSet.size());
+
+        for(String s : hashSet){{
+            System.out.print(s + " ");
+        }
+
+        System.out.println();
+        
+        }
+    }
+}
