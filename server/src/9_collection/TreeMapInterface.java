b6b90b1c45a13ad78355c61331d124ef87b35958
murata0531 <coless115@gmail.com>
2021-03-13 04:04:44 +0900

    TreeMap NavigableMap interface

--- server/src/9_collection/TreeMapInterface.java
+++ server/src/9_collection/TreeMapInterface.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class TeeMapInterface {
+public class TreeMapInterface {
     public static void main(String[] args){
 
         NavigableMap<String,String> map = new TreeMap<String,String>();
