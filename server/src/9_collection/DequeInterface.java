62318ebb704b2362276ff338897224c7ee35e2b5
murata0531 <coless115@gmail.com>
2021-03-13 03:28:12 +0900

    deque interface

--- server/src/9_collection/DequeInterface.java
+++ server/src/9_collection/DequeInterface.java
@@ -3,25 +3,14 @@ import java.util.*;
 public class DequeInterface {
     public static void main(String[] args){
 
-        Deque<String> queue = new ArrayDeque<String>();
+        Deque<String> deque = new ArrayDeque<String>();
 
-        queue.add("1");
-        queue.add("2");
-        queue.add("3");
+        deque.push("1");
+        deque.push("2");
+        deque.push("3");
 
-        System.out.println(queue);
-        System.out.println("element : " + queue.element());
-        System.out.println(queue);
-        System.out.println("remove : " + queue.remove());
-        System.out.println(queue);
-
-        for(String s : queue){
-            queue.poll();
-        }
-
-        System.out.println(queue.peek());
-        // throws exception
-        // System.out.println("element : " + queue.element());
-
+        System.out.println(deque);
+        System.out.println("pop : " + deque.pop());
+        System.out.println(deque);
     }
 }
