cf12bc5784612ee5676ad5d41b985a07bb0c7a41
murata0531 <coless115@gmail.com>
2021-03-13 04:03:13 +0900

    TreeMap NavigableMap interface

---
+++ server/src/9_collection/TreeMapInterface.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class TeeMapInterface {
+    public static void main(String[] args){
+
+        NavigableMap<String,String> map = new TreeMap<String,String>();
+
+        map.put("1111","itemA");
+        map.put("2222","itemB");
+        map.put("3333","itemC");
+        map.put("4444","itemD");
+
+        String key = "2000";
+
+        if(map.containsKey(key)){
+            System.out.println("get() : " + map.get(key));
+        }else {
+            System.out.println("higherKey() : " + map.higherKey(key));
+            System.out.println("lowerKey() : " + map.lowerKey(key));
+        }
+        
+        NavigableMap<String,String> sub = map.subMap("2000",true,"3500",true);
+
+        System.out.println("2000 - 3500 : " + sub);
+    }
+}
