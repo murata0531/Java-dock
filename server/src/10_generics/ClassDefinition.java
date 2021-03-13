f7c19b24593ad5e572d0b3dc8430f9a69530279f
murata0531 <coless115@gmail.com>
2021-03-14 07:06:39 +0900

    class definition

---
+++ server/src/10_generics/ClassDefinition.java
@@ -0,0 +1,32 @@
+class Gen<T> {
+
+    private T var1;
+
+    public Gen(T var1){
+        this.var1 = var1;
+    }
+
+    public T getVar1(){
+        return var1;
+    }
+
+    public void setVar1(T var1){
+        this.var1 = var1;
+    }
+}
+
+public class ClassDefinition {
+    public static void main(String[] args){
+
+        Gen<String> g1 = new Gen<>("ABC");
+        System.out.println(g1.getVar1());
+        g1.setVar1("DEF");
+        System.out.println(g1.getVar1());
+
+        Gen<Integer> g2 = new Gen<>(1);
+        System.out.println(g2.getVar1());
+        g2.setVar1(2);
+        System.out.println(g2.getVar1())
+
+    }
+}
