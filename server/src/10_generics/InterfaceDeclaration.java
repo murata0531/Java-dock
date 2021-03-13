38ff24895e3b030089c4a957632929034bde304c
murata0531 <coless115@gmail.com>
2021-03-14 07:39:40 +0900

    interface declaration

---
+++ server/src/10_generics/InterfaceDeclaration.java
@@ -0,0 +1,21 @@
+interface MyIn<T> {
+    void method(T t);
+}
+
+class Foo implements MyIn<String>{
+    public void method(String s){
+        System.out.println(s);
+    }
+}
+public class InterfaceDeclaration {
+    public static void main(String[] args){
+
+        new Foo.method("ABC");
+
+        new MyIn<Integer>() {
+            public void method(Integer i){
+                System.out.println(i);
+            }
+        }.method(100);
+    }
+}
