e6dd8678ee9242b46b662f508be621afe88217a9
murata0531 <coless115@gmail.com>
2021-03-15 02:06:00 +0900

    wildcard generics2

--- server/src/10_generics/WildcardGenerics2.java
+++ server/src/10_generics/WildcardGenerics2.java
@@ -1,29 +1,47 @@
-class Gen<T> {
+import java.util.*;
 
-    private T var;
-
-    public Gen(T var){
-        this.var = var;
+class X {
+    public String toString(){
+        return "X";
     }
+}
 
-    public T getVar(){
-        return var;
+class Y extends X {
+    public String toString(){
+        return "Y";
     }
 }
 
 public class WildcardGenerics2 {
-    public static void main(String[] args){
 
-        int val = Integer.parseInt(args[0]);
+    public static void method1(List<? extends X> list){
 
-        if(val == 1){
-            print(new Gen<>(100));
-        }else {
-           print(new Gen<>("ABC")); 
-        }
+        // Compile error: When using wildcards, the method does not know the type of the object it receives as an argument until runtime.
+        // list.add(new X());
+        // list.add(new Y());
+        System.out.print(list.get(0) + " ");
     }
 
-    static void print(Gen<?> obj){
-        System.out.println(obj.getVar());
+    public static void method2(List<? super Y> list){
+
+        //Compile OK : Only objects of the same type as the type specified in "super" are possible
+        list.add(new Y());
+        System.out.print(list.get(0) + " ");
+    }
+
+    public static void main(String[] args){
+
+        List<X> l1 = new ArrayList<>();
+        l1.add(new X());
+
+        List<Y> l2 = new ArrayList<>();
+        l1.add(new Y());
+
+        method1(l1);
+        method1(l2);
+        method2(l1);
+        method2(l2);
+
+
     }
 }
