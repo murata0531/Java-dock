833169eaad938c15fae5f0974e9c214c5e0f8ade
murata0531 <coless115@gmail.com>
2021-03-16 02:16:54 +0900

    comparator interface

--- server/src/10_generics/ComparatorInterface.java
+++ server/src/10_generics/ComparatorInterface.java
@@ -28,5 +28,35 @@ class MyRule implement Comparator<Employee> {
 public class ComparatorInterface {
     public static void main(String[] args){
 
+        Employee e1 = new Employee("taro",20);
+        Employee e2 = new Employee("takayuki",10);
+        Employee e3 = new Employee("hiromi",50);
+
+        ArrayList<Employee> ary = new ArrayList<>();
+        ary.add(e1);
+        ary.add(e2);
+        ary.add(e3);
+
+        System.out.println("Index order");
+        print(ary);
+
+        System.out.println("MyRule order(ASC)");
+        Collections.sort(ary,new MyRule());
+        print(ary);
+
+        System.out.println("Name length order(ASC)");
+        Collections.sort(ary,new Comparator<Employee>(){
+            public int comare(Employee o1,Employee o2){
+                return e1.getName().length() - o2.getName().length();
+            }
+        });
+
+        print(ary);
+    }
+
+    public static void print(ArrayList<Employee> ary){
+        for(Employee obj : ary){
+            System.out.println(obj.getId() + "" + obj.getName());
+        }
     }
 }
