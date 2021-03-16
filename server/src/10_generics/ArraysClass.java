7c2cf0be01ce38c51a389abddf94c23ed1e7bc0c
murata0531 <coless115@gmail.com>
2021-03-17 02:01:49 +0900

    Arrays Class

--- server/src/10_generics/ArraysClass.java
+++ server/src/10_generics/ArraysClass.java
@@ -4,18 +4,23 @@ public class ArraysClass {
     public static void main(String[] args){
 
         int[] ary1 = {3,1,2};
-
         print(ary1);
-
         Arrays.sort(ary1);
-
         print(ary1);
 
         Object[] ary2 = {"aa",10};
-
         //class cast exception
         // Arrays.sort(ary2);
 
+        Integer[] ary3 = {1,2,3};
+        List<Integer> list = Arrays.asList(ary3);
+        list.set(2,4);
+        //Elements cannot be added or removed
+        // list.add(5);
+
+        list.forEach(l -> System.out.print(l + " "));
+
+        System.out.println();
     }
 
     public static void print(int[] ary){
