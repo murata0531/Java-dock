c9129a5e4c0eff9c989ccfd660f9f233874f4612
murata0531 <coless115@gmail.com>
2021-03-18 01:30:13 +0900

    Functional Interface

---
+++ server/src/11_FunctionalInterface&Rambda/FunctionalInterface.java
@@ -0,0 +1,14 @@
+class MyFunc implements Function<String,String> {
+    public String apply(String str){
+        return "Hello " + str;
+    }
+}
+
+public class FunctionalInterface {
+    public static void main(String[] args){
+
+        MyFunc obj = new MyFunc();
+        String str = obj.apply("Yamada");
+        System.out.println(str);
+    }
+}
