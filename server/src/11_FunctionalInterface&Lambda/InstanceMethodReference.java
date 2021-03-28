e7df09f273f9ab47096da51faa6fc54050311bfa
murata0531 <coless115@gmail.com>
2021-03-28 21:48:01 +0900

    Instance Method Reference

--- server/src/11_FunctionalInterface&Lambda/InstanceMethodReference.java
+++ server/src/11_FunctionalInterface&Lambda/InstanceMethodReference.java
@@ -24,5 +24,21 @@ public class InstanceMethodReference {
         UnaryOperator<String> obj2 = String::toUpperCase;
 
         System.out.println(obj2.apply("YAMADA"));
+
+        //lambda
+        BiFunctuon<String,Integer,Caracter> bf1 = (s,i) -> s.charAt(i);
+        System.out.println(bf1.apply("Java",2));
+
+        BiFunctuon<Integer,String,Caracter> bf2 = (i,s) -> s.charAt(i);
+        System.out.println(bf2.apply(2,"Java"));
+
+        // instance method reference
+        BiFunctuon<String,Integer,Caracter> bf3 = String::charAt;
+        System.out.println(bf1.apply("Java",2));
+
+        //compile error
+        // = BiFunctuon<Integer,String,Caracter> bf2 = (i,s) -> i.charAt(s);
+        // BiFunctuon<Integer,String,Caracter> bf = String::charAt;
+        // System.out.println(bf1.apply(2,"Java"));
     }
 }
