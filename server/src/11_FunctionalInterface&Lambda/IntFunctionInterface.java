cc9fa5f9faedca9e89ef8eb1a21d25b3cb4ef985
murata0531 <coless115@gmail.com>
2021-03-30 16:41:42 +0900

    IntFunction Interface

--- server/src/11_FunctionalInterface&Lambda/IntFunctionInterface.java
+++ server/src/11_FunctionalInterface&Lambda/IntFunctionInterface.java
@@ -4,31 +4,21 @@ import java.util.function.*;
 public class IntFunctionInterface {
     public static void main(String[] args){
 
-        //lambda
-        // Supplier<Foo> obj1 = () -> new Foo();
-        Supplier<Foo> obj1 = Foo::new;
-        System.out.println(obj1.get().a);
+        // lambda
+        IntFunction<String> l1 = Size -> new String[size];
+        //method reference
+        IntFunction<String> f1 = String[]::new;
+        String[] ans1 = f1.apply(5);
 
         //lambda
-        // Function<Integer,Foo> obj2 = i -> new Foo(i);
-        Function<Integer,Foo> obj2 = Foo::new;
-        System.out.println(obj2.apply(10).a);
+        IntFunction<Double> f2 = i -> Math.random() * i;
+        Double ans2 = f2.apply(10);
 
-        //lambda
-        // Supplier<List<Foo>> obj3 = () ->new ArrayList<>();
-        Supplier<List<Foo>> obj3 = ArrayList::new;
-        System.out.println(obj3.get().size());
+        //This is compile error : Becouse the return value is Object type
+        // IntFunction f2 = i -> Math.random() * i;
 
 
-        Set<Foo> set = Set.of(new Foo(10), new Foo(20));
-
-        //lamda
-        // Function<Set<Foo>, List<Foo>> obj4 = (s) -> new ArrayList<>(s);
-        Function<Set<Foo>, List<Foo>> obj4 = ArrayList::new;
-        System.out.println(obj4.apply(set).size());
-
-        // arrays
-        Function<Integer,String[]> obj5 = String[]::new;
-        System.out.println(obj5.apply(5).length);
+        IntSupplier f3 = "Java"::length;
+        int ans3 = f3.getAsInt();
     }
 }
