acc5ef6eb704c24b94a0d45d3779dfbcda48566b
murata0531 <coless115@gmail.com>
2021-04-04 23:00:26 +0900

    reduce()

---
+++ server/src/12_stream_api/Reduce.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class Reduce {
+    public static void main(String[] args){
+
+        long result1 = Stream.of("a","b","c").count();
+        System.out.println(result1);
+
+        Stream<String> stream1 = Stream.of("a","b","c");
+        stream1.forEach(System.out::print);
+
+        System.out.println();
+
+        // This is compile error : Because stream interface does not inherit "iterable"
+        // for(String s : stream1){
+        //     System.out.print(s);
+        // }
+    }
+}
