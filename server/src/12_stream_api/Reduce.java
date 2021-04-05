1e95de1f440b8fe07420a3b7718e59325cbd90fb
murata0531 <coless115@gmail.com>
2021-04-05 23:41:58 +0900

    reduce()

--- server/src/12_stream_api/Reduce.java
+++ server/src/12_stream_api/Reduce.java
@@ -11,5 +11,10 @@ public class Reduce {
         //int result = stream.reduce(0,Integer::sum);
 
         System.out.println(result);
+
+        BinaryOperator<Integer> operator = (a,b) -> a + b;
+        Stream<Integer> stream1 = Stream.of(10,20,30);
+        Optional<Integer> result = stream1.reduce(operator);
+        result.ifPresent(System.out::println);
     }
 }
