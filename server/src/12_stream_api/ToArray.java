45605ab616e3af750803067e2441f60d1f3b0a08
murata0531 <coless115@gmail.com>
2021-04-06 23:38:36 +0900

    toArray()

--- server/src/12_stream_api/ToArray.java
+++ server/src/12_stream_api/ToArray.java
@@ -5,10 +5,10 @@ import java.util.stream.*;
 public class ToArray {
     public static void main(String[] args){
 
-       int[] array1 = IntStream.range(1,10).ToArray();
-       int[] array2 = IntStream.rangeClosed(1,10).ToArray();
-       Object[] array3 = Stream.of("a","b").ToArray();
-       String[] array4 = Stream.of("a","b").ToArray(String::new);
+       int[] array1 = IntStream.range(1,10).toArray();
+       int[] array2 = IntStream.rangeClosed(1,10).toArray();
+       Object[] array3 = Stream.of("a","b").toArray();
+       String[] array4 = Stream.of("a","b").toArray(String::new);
 
         System.out.println("array1 : " + Arrays.toString(array1));
         System.out.println("array2 : " + Arrays.toString(array2));
