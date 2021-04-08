986e36790ff07732f3ffac9211ab9633a07150ea
murata0531 <coless115@gmail.com>
2021-04-08 23:20:58 +0900

    findFirst(), findAny()

--- server/src/12_stream_api/FindMethod.java
+++ server/src/12_stream_api/FindMethod.java
@@ -15,9 +15,10 @@ public class FindMethod {
         System.out.println("result1 : " + result1.get());
         System.out.println("result2 : " + result2.get());
 
+        //empty data
         Stream<String> stream = Stream.empty();
 
-         Optional<String> result3 = stream.findFirst();
-         result3.ifPresent(r -> System.out.println("result3 : " + r));
+        Optional<String> result3 = stream.findFirst();
+        result3.ifPresent(r -> System.out.println("result3 : " + r));
     }
 }
