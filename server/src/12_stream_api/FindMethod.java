8239827f1c6f7701278a8a48ee45dd2e1a3ca75f
murata0531 <coless115@gmail.com>
2021-04-08 23:11:36 +0900

    findFirst(), findAny()

--- server/src/12_stream_api/FindMethod.java
+++ server/src/12_stream_api/FindMethod.java
@@ -6,30 +6,13 @@ public class FindMethod {
     public static void main(String[] args){
 
         //data source
-        List<String> data = Arrays.asList("aaa","bb","c");
+        List<String> data = Arrays.asList("c","a");
+        //findFirst()
+        Optional<String> result1 = data.stream().findFirst();
+        //findAny()
+        Optional<String> result2 = data.stream().findAny();
 
-        //Maximum value
-        Optional<String> result1 = data.stream().max(Comparator.naturalOrder());
-        //Number of characters
-        Optional<String> result2 = data.stream().max((d1,d2) -> d1.length() - d2.length());
-
-        result1.ifPresent(System.out::println);
-        //ok
-        // System.out.println(result1.get());
-        result2.ifPresent(System.out::println);
-        //ok
-        // System.out.println(result2.get());
-
-        //Minimum value
-        Optional<String> result3 = data.stream().min(Comparator.naturalOrder());
-        //Number of characters
-        Optional<String> result4 = data.stream().min((d1,d2) -> d1.length() - d2.length());
-
-        result3.ifPresent(System.out::println);
-        //ok
-        // System.out.println(result3.get());
-        result4.ifPresent(System.out::println);
-        //ok
-        // System.out.println(result4.get());
+        System.out.println("result1 : " + result1.get());
+        System.out.println("result2 : " + result2.get());
     }
 }
