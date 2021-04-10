e763ef2d0e9035eddca3505a297a6787a7eb0f2a
murata0531 <coless115@gmail.com>
2021-04-10 23:23:02 +0900

    limit(), skip()

--- server/src/12_stream_api/MapMethod.java
+++ server/src/12_stream_api/MapMethod.java
@@ -5,27 +5,10 @@ import java.util.stream.*;
 public class MapMethod {
     public static void main(String[] args){
 
-        //iterate() : generate unlimited stream 
-        // => limit()
-        IntStream.iterate(1,n -> n + 1).limit(10L).forEach(x -> System.out.print(x + " "));
-
-        System.out.println();
-
-        //rangeClosed() : generate 10 limited stream 
-        // => skip(5L)
-        IntStream.rangeClosed(1,10).skip(5L).forEach(x -> System.out.print(x + " "));
-
-        System.out.println();
-
-        //skip(100L) => limit(5L)
-        IntStream.iterate(1,n -> n + 1).skip(100L).limit(5L).forEach(x -> System.out.print(x + " "));
-
-        System.out.println();
-
-        //generate() : generate unlimited stream
-        Stream<String> stream = Stream.generate(() -> "Java");
-        //limit(3L)
-        stream.limit(3L).forEach(x -> System.out.print(x + " "));
+        // Stream<String> => Stream<String>
+        Stream<String> stream1a = Stream.of("yuki","akko","ami");
+        Stream<String> stream1b = stream1a.map(s -> s.toUpperCase());
+        stream1b.forEach(x -> System.out.print(x + " "));
 
         System.out.println();
     }
