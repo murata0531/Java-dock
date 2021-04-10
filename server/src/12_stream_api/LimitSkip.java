b9f12b3cb37109c3a48e4f06effb738e1f073f86
murata0531 <coless115@gmail.com>
2021-04-10 22:59:55 +0900

    limit(), skip()

--- server/src/12_stream_api/LimitSkip.java
+++ server/src/12_stream_api/LimitSkip.java
@@ -5,8 +5,13 @@ import java.util.stream.*;
 public class LimitSkip {
     public static void main(String[] args){
 
-        //iterate()
+        //iterate(), limit()
         IntStream.iterate(1,n -> n + 1).limit(10L).forEach(x -> System.out.print(x + " "));
+
+        System.out.println();
+
+        //rangeClosed(), skip()
+        IntStream.rangeClosed(1,10).skip(5L).forEach(x -> System.out.print(x + " "));
 
         System.out.println();
     }
