84d1b4b4e878d9c2e79fa118ac707b7cf24204f5
murata0531 <coless115@gmail.com>
2021-04-11 23:00:52 +0900

    sorted()

--- server/src/12_stream_api/Sorted.java
+++ server/src/12_stream_api/Sorted.java
@@ -5,27 +5,10 @@ import java.util.stream.*;
 public class Sorted {
     public static void main(String[] args){
 
-        // data sources
-        List<Integer> data1 = Arrays.asList(10);
-        List<Integer> data2 = Arrays.asList(20,30);
-        List<Integer> data3 = Arrays.asList(40,50,60);
-
-        List<List<Integer>> dataList = Arrays.asList(data1,data2,data3);
-
-        //map()
-        dataList.stream()
-                .map(data -> data.stream())
-                .forEach(l -> {
-                    l.forEach(x -> System.out.print(x + " "));
-                });
-
-        System.out.println();
-
-        //flatMap()
-        dataList.stream()
-                .flatMap(data -> data.stream())
-                .forEach(x -> System.out.print(x + " "));
-
+        Stream.of("yuki","akko","ami")
+              .sorted()
+              .forEach(x -> System.out.print(x + " "));
+              
         System.out.println();
     }
 }
