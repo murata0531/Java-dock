08fd24987fa15e66d087c0c41bc5957d1482b5f1
murata0531 <coless115@gmail.com>
2021-04-11 23:03:20 +0900

    sorted()

--- server/src/12_stream_api/Sorted.java
+++ server/src/12_stream_api/Sorted.java
@@ -5,10 +5,18 @@ import java.util.stream.*;
 public class Sorted {
     public static void main(String[] args){
 
+        //asc
         Stream.of("yuki","akko","ami")
               .sorted()
               .forEach(x -> System.out.print(x + " "));
-              
+
+        System.out.println();
+
+        //desc
+        Stream.of("yuki","akko","ami")
+              .sorted(Comparator.reverseOrder())
+              .forEach(x -> System.out.print(x + " "));
+
         System.out.println();
     }
 }
