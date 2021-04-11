a30b759f7d32e1ca65b29bffa2213c13f099bad6
murata0531 <coless115@gmail.com>
2021-04-11 22:55:56 +0900

    flatMap()

--- server/src/12_stream_api/FlatMap.java
+++ server/src/12_stream_api/FlatMap.java
@@ -7,8 +7,8 @@ public class FlatMap {
 
         // data sources
         List<Integer> data1 = Arrays.asList(10);
-        List<Integer> data1 = Arrays.asList(20,30);
-        List<Integer> data1 = Arrays.asList(40,50,60);
+        List<Integer> data2 = Arrays.asList(20,30);
+        List<Integer> data3 = Arrays.asList(40,50,60);
 
         List<List<Integer>> dataList = Arrays.asList(data1,data2,data3);
 
