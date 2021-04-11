d578985476b0c093db933d3338ac0f5ad7cac04b
murata0531 <coless115@gmail.com>
2021-04-11 23:15:40 +0900

    sorted()

---
+++ server/src/12_stream_api/Peek.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+
+public class Peek {
+    public static void main(String[] args){
+
+        List<String> list = 
+            Stream.of("one","three", "two", "three","four");
+                  .filter(s -> s.length() > 3)
+                  .peek(e -> System.out.println("After the filter : " + e))
+                  .distinct()
+                  .peek(e -> System.out.println("After the distinct : " + e))
+                  .map(String::toUpperCase)
+                  .peek(e -> System.out.println("After the map : " + e))
+                  .collect(Collectors.toList());
+
+    }
+}
