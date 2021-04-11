1ca843f67964a73b5b50c404674da81e606ccf86
murata0531 <coless115@gmail.com>
2021-04-11 22:46:09 +0900

    flatMap()

---
+++ server/src/12_stream_api/FlatMap.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+
+public class FlatMap {
+    public static void main(String[] args){
+
+        // data sources
+        List<Integer> data1 = Arrays.asList(10);
+        List<Integer> data1 = Arrays.asList(20,30);
+        List<Integer> data1 = Arrays.asList(40,50,60);
+
+
+        System.out.println();
+    }
+}
