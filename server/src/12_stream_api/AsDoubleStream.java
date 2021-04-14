3d36815b78b196e807066102e8869fc47f816d97
murata0531 <coless115@gmail.com>
2021-04-14 13:15:29 +0900

    stream : asDoubleStream()

--- server/src/12_stream_api/AsDoubleStream.java
+++ server/src/12_stream_api/AsDoubleStream.java
@@ -5,25 +5,10 @@ import java.util.stream.*;
 public class AsDoubleStream {
     public static void main(String[] args){
 
-        //Stream<String> => Stream<Integer> :map()
-        Stream<String> stream1o = Stream.of("a","b");
-        Stream<Integer> stream1n = stream1o.map(s -> s.length());
-
-        //Stream<String> => IntStream :mapToInt()
-        Stream<String> stream2o = Stream.of("yuki","akko","ami");
-        IntStream stream2n = stream2o.mapToInt(s -> s.length());
-
-        //Stream<Integer> => IntStream :mapToInt()
-        Stream<Integer> stream3o = Stream.of(1,2,3);
-        IntStream stream3n = stream3o.mapToInt(n -> n * 10);
-
-        //Stream<Integer> => Stream<String> :map()
-        Stream<Integer> stream4o = Stream.of(1,2,3);
-        Stream<String> stream4n = stream4o.map(n -> n + "n");
-
-        //IntStream => Stream<String> :mapToObj()
-        IntStream stream5o = IntStream.of(1,2,3);
-        Stream<String> stream5n = stream5o.mapToObj(n -> n + "n");
+        //IntStream<String> => DoubleStream :mapToDouble()
+        IntStream stream1i = IntStream.of(2,3,5);
+        DoubleStream stream1d = stream1i.mapToDouble(n -> n);
+        System.out.println(stream1d.average().getAsDouble());
 
         //IntStream => Stream<Integer> :boxed()
         IntStream stream6o = IntStream.of(1,2,3);
