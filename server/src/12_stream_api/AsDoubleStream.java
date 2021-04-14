896c1e4e29549b898d8d227187bf587b3a10cd0b
murata0531 <coless115@gmail.com>
2021-04-14 13:08:47 +0900

    stream : asDoubleStream()

---
+++ server/src/12_stream_api/AsDoubleStream.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+
+public class AsDoubleStream {
+    public static void main(String[] args){
+
+        //Stream<String> => Stream<Integer> :map()
+        Stream<String> stream1o = Stream.of("a","b");
+        Stream<Integer> stream1n = stream1o.map(s -> s.length());
+
+        //Stream<String> => IntStream :mapToInt()
+        Stream<String> stream2o = Stream.of("yuki","akko","ami");
+        IntStream stream2n = stream2o.mapToInt(s -> s.length());
+
+        //Stream<Integer> => IntStream :mapToInt()
+        Stream<Integer> stream3o = Stream.of(1,2,3);
+        IntStream stream3n = stream3o.mapToInt(n -> n * 10);
+
+        //Stream<Integer> => Stream<String> :map()
+        Stream<Integer> stream4o = Stream.of(1,2,3);
+        Stream<String> stream4n = stream4o.map(n -> n + "n");
+
+        //IntStream => Stream<String> :mapToObj()
+        IntStream stream5o = IntStream.of(1,2,3);
+        Stream<String> stream5n = stream5o.mapToObj(n -> n + "n");
+
+        //IntStream => Stream<Integer> :boxed()
+        IntStream stream6o = IntStream.of(1,2,3);
+        Stream<Integer> stream6n = stream6o.boxed();
+
+    }
+}
