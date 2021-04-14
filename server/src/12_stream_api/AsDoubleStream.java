e554e418da8b9552e28931604a02d438f87d947f
murata0531 <coless115@gmail.com>
2021-04-14 13:21:07 +0900

    stream : asDoubleStream()

--- server/src/12_stream_api/AsDoubleStream.java
+++ server/src/12_stream_api/AsDoubleStream.java
@@ -5,14 +5,15 @@ import java.util.stream.*;
 public class AsDoubleStream {
     public static void main(String[] args){
 
-        //IntStream<String> => DoubleStream :mapToDouble()
+        //IntStream<String> => DoubleStream : mapToDouble()
         IntStream stream1i = IntStream.of(2,3,5);
         DoubleStream stream1d = stream1i.mapToDouble(n -> n);
         System.out.println(stream1d.average().getAsDouble());
 
-        //IntStream => Stream<Integer> :boxed()
-        IntStream stream6o = IntStream.of(1,2,3);
-        Stream<Integer> stream6n = stream6o.boxed();
+        //IntStream<String> => DoubleStream : asDoubleStream()
+        IntStream stream2i = IntStream.of(2,3,5);
+        DoubleStream stream2d = stream1i.asToDoubleStream();
+        System.out.println(stream2d.average().getAsDouble());
 
     }
 }
