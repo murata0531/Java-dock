8335b40f021e73f2c645ed16f573767fd48af7f0
murata0531 <coless115@gmail.com>
2021-04-25 02:17:55 +0900

    Collectors class

--- server/src/12_stream_api/Collectors.java
+++ server/src/12_stream_api/Collectors.java
@@ -5,15 +5,13 @@ import java.util.stream.*;
 public class Collectors {
     public static void main(String[] args){
 
-        //IntStream<String> => DoubleStream : mapToDouble()
-        IntStream stream1i = IntStream.of(2,3,5);
-        DoubleStream stream1d = stream1i.mapToDouble(n -> n);
-        System.out.println(stream1d.average().getAsDouble());
+        //collect()
+        Stream<String> stream1 = Stream.of("ami","akko","yuki");
+        List<String> result1 = stream1.collect(Collectors.toList());
 
-        //IntStream<String> => DoubleStream : asDoubleStream()
-        IntStream stream2i = IntStream.of(2,3,5);
-        DoubleStream stream2d = stream2i.asDoubleStream();
-        System.out.println(stream2d.average().getAsDouble());
+        System.out.println(result1);
+
+        
 
     }
 }
