9c06a4107a6b1589a2bc3572f9594687a9c3ee35
murata0531 <coless115@gmail.com>
2021-04-26 19:37:45 +0900

    toSet(). toMap()

--- server/src/12_stream_api/ToSetToMap.java
+++ server/src/12_stream_api/ToSetToMap.java
@@ -5,14 +5,9 @@ import java.util.stream.*;
 public class ToArray {
     public static void main(String[] args){
 
-       int[] array1 = IntStream.range(1,10).toArray();
-       int[] array2 = IntStream.rangeClosed(1,10).toArray();
-       Object[] array3 = Stream.of("a","b").toArray();
-       String[] array4 = Stream.of("a","b").toArray(String[]::new);
+       Stream<String> stream1 = Stream.of("ami","akko","yuki");
+       Set<String> set = stream1.collect(Collectors.toSet());
 
-        System.out.println("array1 : " + Arrays.toString(array1));
-        System.out.println("array2 : " + Arrays.toString(array2));
-        System.out.println("array3 : " + array3.getClass());
-        System.out.println("array4 : " + array4.getClass());
+       System.out.println(set);
     }
 }
