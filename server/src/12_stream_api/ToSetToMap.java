8193a8046b77546e85b22696c708c6c3b8942898
murata0531 <coless115@gmail.com>
2021-04-26 19:38:31 +0900

    toSet(). toMap()

--- server/src/12_stream_api/ToSetToMap.java
+++ server/src/12_stream_api/ToSetToMap.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.util.function.*;
 import java.util.stream.*;
 
-public class ToArray {
+public class ToSetToMap {
     public static void main(String[] args){
 
        Stream<String> stream1 = Stream.of("ami","akko","yuki");
