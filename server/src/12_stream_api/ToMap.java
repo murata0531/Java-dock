6407a752ecf9d49af65702d2e18970d30083a4a9
murata0531 <coless115@gmail.com>
2021-04-28 14:44:22 +0900

    toMap()

--- server/src/12_stream_api/ToMap.java
+++ server/src/12_stream_api/ToMap.java
@@ -11,13 +11,13 @@ public class ToMap {
         System.out.println(map1);
 
         //Throws IllegalStateException : Because the number of characters is the same, the keys are duplicated
-        Stream<String> stream2 = Stream.of("ami","akko","yuki");
-        Map<Integer,String> map2 = stream1.collect(Collectors.toMap(String::length,s -> s));
+        // Stream<String> stream2 = Stream.of("ami","akko","yuki");
+        // Map<Integer,String> map2 = stream1.collect(Collectors.toMap(String::length,s -> s));
 
-        System.out.println(map2);
+        // System.out.println(map2);
 
         Stream<String> stream3 = Stream.of("ami","akko","yuki");
-        Map<Integer,String> map3 = stream1.collect(Collectors.toMap(String::length,s -> s,(s1,s2) -> s1 + " : " + s2);
+        Map<Integer,String> map3 = stream3.collect(Collectors.toMap(String::length,s -> s,(s1,s2) -> s1 + " : " + s2));
 
         System.out.println(map3);
     }
