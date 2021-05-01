3e9d39fa2d50e8ce0ed21814e7586493c0244aa5
murata0531 <coless115@gmail.com>
2021-05-01 19:48:57 +0900

    partitioningBy()

--- server/src/12_stream_api/PartitioningBy.java
+++ server/src/12_stream_api/PartitioningBy.java
@@ -5,26 +5,8 @@ import java.util.stream.*;
 public class PartitioningBy {
     public static void main(String[] args){
 
-        Stream<String> stream1 = Stream.of("belle","akko","ami","bbb","nao");
-        Map<String,List<String>> map1 = stream1.collect(Collectors.groupingBy(s -> s.substring(0,1)));
-
+        Stream<Integer> stream1 = Stream.of(3,5,7,9);
+        Map<Boolean,List<Integer>> map1 = stream1.collect(Collectors.partittioningBy(s -> s > 5))
         System.out.println(map1);
-
-        Stream<String> stream2 = Stream.of("belle","akko","ami","bbb","nao");
-        Map<String,Set<String>> map2 = stream2.collect(Collectors.groupingBy(s -> s.substring(0,1),Collectors.toSet()));
-
-        System.out.println(map2);
-
-        Stream<String> stream3 = Stream.of("belle","akko","ami","bbb","nao");
-        Map<String,String> map3 = stream3.collect(Collectors.groupingBy(s -> s.substring(0,1),Collectors.joining()));
-
-        System.out.println(map3);
-        System.out.println(map3.getClass());
-
-        Stream<String> stream4 = Stream.of("belle","akko","ami","bbb","nao");
-        Map<String,String> map4 = stream4.collect(Collectors.groupingBy(s -> s.substring(0,1),TreeMap::new, Collectors.joining()));
-
-        System.out.println(map4);
-        System.out.println(map4.getClass());
     }
 }
