2a7ea1d02808d2db1b921e1c80f75fd08937d0df
murata0531 <coless115@gmail.com>
2021-05-01 21:56:31 +0900

    maxBy(), minBy()

--- server/src/12_stream_api/MaxByMinBy.java
+++ server/src/12_stream_api/MaxByMinBy.java
@@ -5,9 +5,18 @@ import java.util.stream.*;
 public class MaxByMinBy {
     public static void main(String[] args){
 
-        //map()
+        //minBy()
         Stream<String> stream1 = Stream.of("ami","akko","yuki");
         Optional<String> result1 = stream1.collect(Collectors.minBy(Comparator.naturalOrder()));
         System.out.println(result1.get());
+
+        //maxBy()
+        Stream<String> stream2 = Stream.of("101","105","106","203","205");
+        Map<String,Optional<String>> result2 = stream2.collect(
+                                                    Collectors.groupingBy(
+                                                        s -> s.substring(0,1),
+                                                        Collectors.maxBy(Comparator.naturalOrder())
+                                                    )
+                                                );
     }
 }
