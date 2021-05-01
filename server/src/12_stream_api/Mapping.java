4dd808b9693f6060feb442cdc2401a75a8efc231
murata0531 <coless115@gmail.com>
2021-05-01 21:09:13 +0900

    mapping()

--- server/src/12_stream_api/Mapping.java
+++ server/src/12_stream_api/Mapping.java
@@ -6,9 +6,13 @@ public class Mapping {
     public static void main(String[] args){
 
         //map()
-       Stream<String> stream1 = Stream.of("ami","akko","yuki");
+        Stream<String> stream1 = Stream.of("ami","akko","yuki");
         String result1 = stream1.map(s -> s.toUpperCase()).collect(Collectors.joining(":"));
         System.out.println(result1);
 
+        //mapping()
+        Stream<String> stream2 = Stream.of("ami","akko","yuki");
+        String result2 = stream2.collect(Collectors.mapping(s -> s.toUpperCase(),Collectors.joining(":")));
+        System.out.println(result2);
     }
 }
