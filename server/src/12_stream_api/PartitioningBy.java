a41bcff2841c9b9b725f5f24398b669ee13c868b
murata0531 <coless115@gmail.com>
2021-05-01 19:54:31 +0900

    partitioningBy()

--- server/src/12_stream_api/PartitioningBy.java
+++ server/src/12_stream_api/PartitioningBy.java
@@ -6,7 +6,7 @@ public class PartitioningBy {
     public static void main(String[] args){
 
         Stream<Integer> stream1 = Stream.of(3,5,7,9);
-        Map<Boolean,List<Integer>> map1 = stream1.collect(Collectors.partittioningBy(s -> s > 5))
+        Map<Boolean,List<Integer>> map1 = stream1.collect(Collectors.partitioningBy(s -> s > 5));
         System.out.println(map1);
     }
 }
