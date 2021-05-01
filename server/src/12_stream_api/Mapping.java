08191edc7cedea2c5d1af5fc5e1e89866dd307d2
murata0531 <coless115@gmail.com>
2021-05-01 20:54:46 +0900

    mapping()

--- server/src/12_stream_api/Mapping.java
+++ server/src/12_stream_api/Mapping.java
@@ -5,16 +5,10 @@ import java.util.stream.*;
 public class Mapping {
     public static void main(String[] args){
 
-        Stream<Integer> stream1 = Stream.of(3,5,7,9);
-        Map<Boolean,List<Integer>> map1 = stream1.collect(Collectors.partitioningBy(s -> s > 5));
-        System.out.println(map1);
+        //map()
+       Stream<String> stream1 = Stream.of("ami","akko","yuki");
+        String result1 = stream1.map(s -> s.toUpperCase()).collect(Collectors.joining(":"));
+        System.out.println(result1);
 
-        Stream<Integer> stream2 = Stream.of(3,5,7,9);
-        Map<Boolean,List<Integer>> map2 = stream2.collect(Collectors.partitioningBy(s -> s > 10));
-        System.out.println(map2);
-
-        Stream<Integer> stream3 = Stream.of(3,5,7,9);
-        Map<Boolean,Integer> map3 = stream3.collect(Collectors.partitioningBy(s -> s > 5,Collectors.summingInt(n -> n)));
-        System.out.println(map3);
     }
 }
