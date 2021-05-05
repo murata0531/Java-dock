3a12c5e201af66c3090554f3920518fd1aa8d925
murata0531 <coless115@gmail.com>
2021-05-05 12:10:51 +0900

    thread

--- server/src/14_thread/SharedThread.java
+++ server/src/14_thread/SharedThread.java
@@ -9,3 +9,20 @@ public class SharedThread {
     }
 }
 
+class Share {
+
+    private int a = 0;
+    private String b;
+
+    public void set(){
+        a++;
+        b = "data";
+        System.out.println("set() a : " + a + " b : " + b);
+    }
+
+    public void print(){
+        a--;
+        b = null;
+        System.out.println("set() a : " + a + " b : " + b);
+    }
+}
