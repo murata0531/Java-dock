e0015120c96d081e2f55e5e4165f0ed7262ab1d3
murata0531 <coless115@gmail.com>
2021-05-05 12:06:10 +0900

    thread

--- server/src/14_thread/SharedThread.java
+++ server/src/14_thread/SharedThread.java
@@ -1,30 +1,11 @@
 public class SharedThread {
     public static void main(String[] args){
 
-        Thread threadA = new Thread(() -> {
-
-            System.out.println("threadA : sleep");
-
-            try{
-                Thread.sleep(5000);
-            }catch(InterruptedException e){
-                System.out.println("threadA : caught an interrupt");
-            }
-
-            System.out.println("threadA : resume");
-
-        });
-
+        Share share = Share();
+        ThreadA threadA = new threadA(share);
+        ThreadB threadB = new threadB(share);
         threadA.start();
-
-        try {
-            System.out.println("main : sleep");
-            Thread.sleep(2000);
-            System.out.println("main : sleep end");
-            threadA.interrupt();
-        }catch(InterruptedException e){
-            System.out.println("main : caught an interrupt");
-        }
+        threadB.start();
     }
 }
 
