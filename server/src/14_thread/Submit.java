4247750c77b719268c01eb821741220e84449278
murata0531 <coless115@gmail.com>
2021-05-06 21:25:51 +0900

    submit

---
+++ server/src/14_thread/Submit.java
@@ -0,0 +1,36 @@
+import java.util.concurrent.*;
+import java.util.*;
+
+public class Submit {
+    public static void main(String[] args){
+
+        ExecutorService service = null;
+
+        try {
+
+            service = Executors.newSingleThreadExecutor();
+
+            System.out.println("service.execute()");
+
+            for(int i = 0; i < 3; i++){
+                service.execute(() -> {
+                    System.out.print("thread task");
+                    for(int a = 0; a < 5; a++){
+                        try {
+                            Thread.sleep(500);
+                            System.out.print(" * ");
+                        }catch(InterruptedException e){
+                            e.printStackTrace();
+                        }
+                    }
+
+                    System.out.println();
+                });
+            }
+        } finally {
+            service.shutdown();
+            System.out.println("ex.shutdown()");
+        }
+        
+    }
+}
