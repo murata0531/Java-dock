092c76d96cf19ac1d5a7ba766807e3f6aab7e7e5
murata0531 <coless115@gmail.com>
2021-05-07 12:34:01 +0900

    ScheduledExectorService

--- server/src/14_thread/ScheduledExecutorServiceInterface.java
+++ server/src/14_thread/ScheduledExecutorServiceInterface.java
@@ -1,13 +1,17 @@
+import java.util.concurrent.*;
+import java.util.*;
+
 public class ScheduledExecutorServiceInterface {
     public static void main(String[] args){
 
+        ScheduledExecutorService service = null;
         try {
-            service = Executors.newSingleThreadExecutor();
+            service = Executors.newSingleThreadScheduledExecutor();
             Runnable task1 = () -> System.out.println("task1");
             Callable<Date> task2 = () -> new Date();
 
             ScheduledFuture<?> result1 = service.schedule(task1,3,TimeUnit.SECONDS);
-            ScheduledFuture<?> result2 = service.schedule(task2,1,TimeUnit.SECONDS);
+            ScheduledFuture<?> result2 = service.schedule(task2,1,TimeUnit.MILLISECONDS);
 
             System.out.println(result2.get());
 
