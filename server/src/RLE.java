0888ae080f4e4e8e41fe1449d2e9d453ededb177
murata0531 <coless115@gmail.com>
2021-05-09 22:51:14 +0900

    run length

--- server/src/RLE.java
+++ server/src/RLE.java
@@ -2,24 +2,28 @@ import java.util.*;
 
 class RLE {
 	public static void main(String[] args){
-        
+
 		Scanner scn = new Scanner(System.in);
+
+        String command = scn.next();
 		char s[] = scn.next().toCharArray();
 		int ansPos = 0;
 		int i, ct;
 		
-		for (i = 1, ct = 1; i < s.length; i++){
-			if (s[i] == s[i-1])
-				ct++;
-			else{
-				System.out.format("%c%d", s[i-1], ct);
-				ct = 1;
-			}
-		}
-		if (s[i-1] == s[i-2])
-			System.out.format("%c%d\n", s[i-1], ct);
-		else
-			System.out.format("%c%d\n", s[i-1], ct);
+		if(command == "compress"){
+            for (i = 1, ct = 1; i < s.length; i++){
+                if (s[i] == s[i-1]){
+                    ct++;
+                }else{
+                    System.out.format("%c%d", s[i-1], ct);
+                    ct = 1;
+                }
+            }
+            if (s[i-1] == s[i-2])
+                System.out.format("%c%d\n", s[i-1], ct);
+            else
+                System.out.format("%c%d\n", s[i-1], ct);
+        }
 	}
 
 }
