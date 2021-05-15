99c0da4076b6ab6e4251a5392c1e88cc25dbe3e8
murata0531 <coless115@gmail.com>
2021-05-15 18:07:06 +0900

    AtomicInteger

--- server/src/14_thread/AtomicInt.java
+++ server/src/14_thread/AtomicInt.java
@@ -26,28 +26,24 @@ class IntegerTest {
 }
 
 public class AtomicInt {
-
-    void exec(CyclicBarrier barrier) {
-        try {
-            System.out.println(Thread.currentThread().getName() + "start");
-            Thread.sleep((int)(Math.random() * 3000));
-            barrier.await();
-            System.out.println(Thread.currentThread().getName() + "end");
-        } catch(BrokenBarrierException | InterruptedException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws InterruptedException {
+        IntegerTest obj = new IntegerTest();
+        exec(()->obj.addSyncInteger());
+        exec(()->obj.addAtomicInteger());
+        obj.showData(); 
     }
 
-    public static void main(String[] args) {
-
+    private static void exec(Runnable task) throws InterruptedException {
         ExecutorService service = null;
-
         try {
-            service = Executors.newFixedThreadPool(4);
-            CyclicBarrier barrier = new CyclicBarrier(2, () -> System.out.println("task "));
-            for(int i = 0; i < 4; i++){
-                service.execute(() -> new CyclicBarrierClass().exec(barrier));
+            service = Executors.newFixedThreadPool(100);
+
+            for(int i = 0; i < 10000; i++){
+                service.submit(task);
             }
+
+            service.awaitTermination(10, TimeUnit.SECONDS);
+            
         } finally {
             if(service != null) service.shutdown(); 
         }
