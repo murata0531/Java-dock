1e6d9060513f9ecea18d12ba862d06fdc4de8a85
murata0531 <coless115@gmail.com>
2021-05-19 19:41:43 +0900

    concurrent

--- server/src/14_thread/Concurrent.java
+++ server/src/14_thread/Concurrent.java
@@ -5,30 +5,19 @@ import java.util.concurrent.*;
 public class Concurrent {
     public static void main(String[] args) {
 
-        List<String> data = Arrays.asList("orange", "banana", "lemon");
-
-        List<String> list = 
-                data.parallelStream()
-                    .collect(() -> new CopyOnWriteArrayList<>(),
-                            (plist, s) -> plist.add(s.toUpperCase()),
-                            (alist, blist) -> alist.addAll(blist));
-
-        for(String s : list){
-            System.out.print(s + " ");
-        }
-    
-        System.out.println();
-
-        Set<String> set = 
-                data.parallelStream()
-                    .collect(CopyOnWriteArraySet::new,
-                            Set::add,
-                            Set::addAll);
-
-        for(String e : set){
-            System.out.print(e + " ");
-        }
-
-        System.out.println();
+        Stream<String> stream1 = Stream.of("belle", "akko", "ami", "bob", "nao").parallel();
+        Map<String, List<String>> map1= stream1.collect(Collectors.groupingByConcurrent(s -> s.substring(0, 1)));
+        System.out.println(map1);
+        System.out.println("map1のクラス名 :" + map1.getClass());
+        
+        Stream<String> stream2 = Stream.of("nao", "akko", "ami").parallel();
+        Map<Integer, String> map2 = 
+                stream2.collect(Collectors.toConcurrentMap(
+                                        String::length,
+                                        s -> s,
+                                        (s1, s2) -> s1 + " : " + s2));
+                                        
+        System.out.println(map2);
+        System.out.println("map2のクラス名 :" + map2.getClass());
     }
 }
