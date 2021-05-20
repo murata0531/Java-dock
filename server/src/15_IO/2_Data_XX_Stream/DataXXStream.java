171e1b655d2472a599ceb6ccc74a100fe6caf2fa
murata0531 <coless115@gmail.com>
2021-05-20 19:42:07 +0900

    data xx stream

--- server/src/15_IO/2_Data_XX_Stream/DataXXStream.java
+++ server/src/15_IO/2_Data_XX_Stream/DataXXStream.java
@@ -5,7 +5,9 @@ public class DataXXStream {
         try (DataOutputStream dos = new DataOutputStream(new FileOutputStream("data.txt"));
             DataInputStream dis = new DataInputStream(new FileInputStream("data.txt"))) {
             
-            dos.writeInt(100); dos.writeUTF("tanaka"); dos.writeUTF("田中");
+            dos.writeInt(100); dos.writeUTF("tanaka");
+            dos.writeUTF("田中");
+            
             System.out.println(dis.readInt());
             System.out.println(dis.readUTF());
             System.out.println(dis.readUTF());
